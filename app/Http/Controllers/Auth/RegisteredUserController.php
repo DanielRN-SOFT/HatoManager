f966f83564f84cc72b4e4f93c8579d62daa844e0
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-05-28 01:27:08 -0500

    Se arregla pequeño bug en registeredUserController para validar creacion de cuenta por parte de veterinario por medio de link  de invitacion

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\VeterinarianController;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -22,7 +23,9 @@ class RegisteredUserController extends Controller
      */
     public function create(): Response
     {
-        return Inertia::render('Auth/Register');
+        return Inertia::render('Auth/Register', [
+            'veterinarianToken' => request('vet_token'),
+        ]);
     }
 
     /**
@@ -44,11 +47,19 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        $user->assignRole('comprador');
+        $vetToken = $request->input('veterinarian_token');
+
+        if (!$vetToken) {
+            $user->assignRole('comprador');
+        }
 
         event(new Registered($user));
 
         Auth::login($user);
+
+        if ($vetToken) {
+            VeterinarianController::acceptTokenInvitation($user, $vetToken);
+        }
 
         return redirect(route('dashboard', absolute: false));
     }
