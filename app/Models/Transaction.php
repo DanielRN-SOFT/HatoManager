6c433640ec8cf5432631c315dab7f90ad532c24d
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-06-13 21:00:17 -0500

    Migraciones para transacciones

---
+++ app/Models/Transaction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'order_id',
+        'deposit_id',
+        'transaction_id',
+        'wompi_id',
+        'internal_reference',
+        'transaction_date',
+        'moneda',
+        'amount',
+        'transaction_status',
+        'transaction_type',
+    ];
+
+    protected $casts = [
+        'transaction_date' => 'datetime',
+        'amount' => 'decimal:2',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function deposit()
+    {
+        return $this->belongsTo(Deposit::class);
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+}
