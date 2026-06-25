7264c19f410871556ba4a0cf6f09e2781556da16
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-06-25 16:29:00 -0500

    Email plantilla modificada

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,16 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+    <tr>
+        <td class="content-cell" align="center">
+            <p>
+                © {{ date('Y') }} {{ config('app.name') }}. Todos los derechos reservados.
+            </p>
+            <p style="margin-top: 4px;">
+                El mercado ganadero que Colombia necesitaba.
+            </p>
+        </td>
+    </tr>
+</table>
+</td>
+</tr>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">
+            <img  src="/images/HatoManager-logo.png"
+                 alt="{{ config('app.name') }}"
+                 style="max-height: 44px; width: auto; display: block; margin: 0 auto;">
+        </a>
+    </td>
+</tr>
