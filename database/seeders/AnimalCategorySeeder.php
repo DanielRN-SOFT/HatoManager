8b04ae5ad63bd403ed77fa3af8386e1b48522a0d
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-07-05 14:08:30 -0500

    seeder para categorias de animales corregido

--- database/seeders/AnimalCategorySeeder.php
+++ database/seeders/AnimalCategorySeeder.php
@@ -10,15 +10,21 @@ class AnimalCategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            'Bovino',
-            'Ovino',
-            'Caprino',
-            'Aviar',
-            'Porcino',
+            'Ternero',      // Macho, 0-12 meses
+            'Ternera',      // Hembra, 0-12 meses
+            'Novillo',      // Macho, 12-24 meses
+            'Novilla',      // Hembra, 12-24 meses
+            'Torete',       // Macho, 24-36 meses
+            'Toro',         // Macho adulto reproductor
+            'Vaca',         // Hembra adulta reproductora
+            'Vaca Horra',   // Hembra adulta no gestante/no productiva
+            'Buey',         // Macho castrado
         ];
+
         foreach ($categories as $name) {
             AnimalCategory::firstOrCreate(['name' => $name]);
         }
+
         $this->command->info(count($categories) . ' categorías de animales creadas.');
     }
 }
